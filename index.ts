82fa647f9a76a6017097e3d7cf7c65cdd351d608
IsraCoder <IZughyer@gmail.com>
2023-10-08 21:33:10 +0300

    basic router setup

--- index.ts
+++ index.ts
@@ -2,6 +2,13 @@ import "dotenv/config";
 import express from "express";
 import db from "./db/index.js";
 import baseLogger from "./logger.js";
+import { error404Handler } from "./middlewares/errorHandlers/genericHandler.js";
+import userRouter from "./routes/users.js";
+import bookRouter from "./routes/books.js";
+import genresRouter from "./routes/genres.js";
+import librariesRouter from "./routes/libraries.js";
+import permissionsRouter from "./routes/permissions.js";
+import rolesRouter from "./routes/roles.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -9,6 +16,14 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server UP!");
 });
+
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+app.use("/genres", genresRouter);
+app.use("/libraries", librariesRouter);
+app.use("/permissions", permissionsRouter);
+app.use("/roles", rolesRouter);
+app.use(error404Handler);
 
 app.use((req, res) => {
   res.status(404).send("You requested something I don't have :(");
